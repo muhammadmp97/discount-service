e8cb4bc38fc1584473b09225df11f7152ca01b7a
Muhammad M <muhammadmp1997@gmail.com>
2025-02-28 23:19:28 +0330

    add routes

---
+++ internal/api/handler/discount.go
@@ -0,0 +1,35 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func List(c *gin.Context) {
+	// TODO
+}
+
+func Show(c *gin.Context) {
+	// TODO
+}
+
+func Create(c *gin.Context) {
+	// TODO
+}
+
+func Delete(c *gin.Context) {
+	// TODO
+}
+
+func Update(c *gin.Context) {
+	// TODO
+}
+
+func Verify(c *gin.Context) {
+	// TODO
+}
+
+func Apply(c *gin.Context) {
+	// TODO
+}
+
+func Cancel(c *gin.Context) {
+	// TODO
+}
--- internal/api/route.go
+++ internal/api/route.go
@@ -8,4 +8,16 @@ import (
 
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/ping", handler.Ping)
+
+	// Admin endpoints
+	r.GET("/discounts", handler.List)
+	r.GET("/discounts/:id", handler.Show)
+	r.POST("/discounts", handler.Create)
+	r.DELETE("/discounts/:id", handler.Delete)
+	r.PUT("/discounts/:id", handler.Update)
+
+	// User endpoints
+	r.GET("/discounts/:code/verify", handler.Verify)
+	r.GET("/discounts/:code/apply", handler.Apply)
+	r.GET("/discounts/:code/cancel", handler.Cancel)
 }
